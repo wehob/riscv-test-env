9b11738a1dec18bd6031d3e4adb0e77be9bea687
wehob <z897899878@gmail.com>
2021-01-06 22:00:27 +0800

    scall6

--- v/riscv_test.h
+++ v/riscv_test.h
@@ -34,16 +34,16 @@ write_tohost:                                                         \
         addi  a5,a5,0;                                                  \
         sw TESTNUM, 0(a5);                                              \
         j write_tohost;                                                 \
-                
+
 //-----------------------------------------------------------------------
 // Pass/Fail Macro
 //-----------------------------------------------------------------------
 
 #undef RVTEST_PASS
-#define RVTEST_PASS li a0, 1; write_tohost
+#define RVTEST_PASS li a0, 1;j write_tohost
 
 #undef RVTEST_FAIL
-#define RVTEST_FAIL sll a0, TESTNUM, 1; 1:beqz a0, 1b; or a0, a0, 1; write_tohost;
+#define RVTEST_FAIL sll a0, TESTNUM, 1; 1:beqz a0, 1b; or a0, a0, 1;j write_tohost;
 
 //-----------------------------------------------------------------------
 // Data Section Macro
